7e2a0dc5fda69a0f7dbd4db684afdd1e438aeabd
Joel <camacho-joel@hotmail.com>
2019-09-03 14:39:26 +0200

    Creating EmployeeTable component and importing it

---
+++ vue-employee-database/src/components/EmployeeTable.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="employee-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Kim Obasogie</td>
+          <td>k.obasogie@gmail.com</td>
+        </tr>
+        <tr>
+          <td>Julie Pladys</td>
+          <td>j.pladys@gmail.com</td>
+        </tr>
+        <tr>
+          <td>Lynn Brants</td>
+          <td>l.brants@gmail.com</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "employee-table"
+};
+</script>
+
+<style scoped></style>
