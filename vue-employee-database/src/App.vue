908d8d76965d911badb1b87211a336f02cc55f61
Joel <camacho-joel@hotmail.com>
2019-09-03 15:32:31 +0200

    creating methode to update array of employees when submiting a new employee

--- vue-employee-database/src/App.vue
+++ vue-employee-database/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="small-container">
     <h1>Employees</h1>
-    <employee-form />
+    <employee-form @add:employee="addEmployee" />
     <employee-table :employees="employees" />
   </div>
 </template>
@@ -35,6 +35,18 @@ export default {
         }
       ]
     };
+  },
+  methods: {
+    addEmployee(employee) {
+      const lastId =
+        this.employees.length > 0
+          ? this.employees[this.employees.length - 1].id
+          : 0;
+      const id = lastId + 1;
+      const newEmployee = { ...employee, id };
+
+      this.employees = [...this.employees, newEmployee];
+    }
   }
 };
 </script>
