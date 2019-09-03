18beccfbeb6596b0d77119f662d8efbd372b69ce
Joel <camacho-joel@hotmail.com>
2019-09-03 15:09:09 +0200

    creating and importing EmployeeForm component

---
+++ vue-employee-database/src/components/EmployeeForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="employee-form">
+    <form>
+      <label>Employee name</label>
+      <input type="text" />
+      <label>Employee Email</label>
+      <input type="text" />
+      <button>Add Employee</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "employee-form"
+};
+</script>
+
+<style scoped>
+label {
+  color: #05386b;
+}
+form {
+  margin-bottom: 2rem;
+}
+button {
+  background-color: #379683;
+}
+</style>
